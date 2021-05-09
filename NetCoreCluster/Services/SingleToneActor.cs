57396b0f1c1e26588983138e37d98f72c1a5d4cd
psmon <psmon@live.co.kr>
2021-05-09 17:54:01 +0900

    싱글톤(어드민)액터 추가

---
+++ NetCoreCluster/Services/SingleToneActor.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using NetCoreCluster.Config;
+
+namespace NetCoreCluster.Services
+{
+    public class SingleToneActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter logger = Context.GetLogger();
+
+        public SingleToneActor()
+        {
+            logger.Debug("Create SingleToneActor");
+
+            ReceiveAsync<string>(async msg =>
+            {
+                logger.Debug($"SingleToneActor:{msg}");
+
+                AkkaLoadEx.ActorSelect("ClusterWorkerPoolActor").Tell("Task");
+
+            });
+        }
+    }
+}
--- NetCoreCluster/StartupService.cs
+++ NetCoreCluster/StartupService.cs
@@ -1,5 +1,6 @@
 ﻿using Akka.Actor;
 using Akka.Routing;
+using AkkaDotModule.Config;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using NetCoreCluster.Config;
@@ -23,22 +24,35 @@ namespace NetCoreCluster
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-
+            var roles = Environment.GetEnvironmentVariable("CLUSTER_ROLES");
             var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             SystemNameForCluster = Environment.GetEnvironmentVariable("ACTORSYSTEM");            
 
             var akkaConfig = AkkaLoadEx.Load(envName);
             var actorSystem = ActorSystem.Create(SystemNameForCluster, akkaConfig);
             
-            var worker = AkkaLoadEx.RegisterActor(
+            AkkaLoadEx.RegisterActor(
                 "ClusterWorkerPoolActor",
                 actorSystem.ActorOf(Props.Create<WorkActor>()
                         .WithRouter(FromConfig.Instance),
                         "cluster-workerpool"
             ));
 
-            for(int i=0;i<10;i++) worker.Tell($"TestMessage{i}");
 
+
+            if (roles.Contains("AdminNode"))
+            {
+                string singleToneRole = "AdminNode";
+                actorSystem.BootstrapSingleton<SingleToneActor>("SingleToneActor", singleToneRole);
+                var singleToneActor = actorSystem.BootstrapSingletonProxy("SingleToneActor", singleToneRole, "/user/SingleToneActor", "singleToneActorProxy");
+                AkkaLoadEx.RegisterActor("SingleToneActor", singleToneActor);
+
+                actorSystem
+                    .Scheduler
+                    .ScheduleTellRepeatedly(TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(1),
+                    singleToneActor, "이벤트 발생..", ActorRefs.NoSender);
+            }
 
             return Task.CompletedTask;
         }
